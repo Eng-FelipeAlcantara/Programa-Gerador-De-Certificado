f39d23b7a6c6c600e64ac88ab0edc6231d6afcbb
Felipe Félix de Alcântara <112891826+Eng-FelipeAlcantara@users.noreply.github.com>
2022-09-14 12:21:37 -0300

    Gerador de certificados de particiapação
    Programa gerador de certificados de participação em evento. 
    Programa desenvolvido em Python.

---
+++ geradorDeCertificado.py
@@ -0,0 +1,8 @@
+#Programa gerador de certificado de participação em eventos
+responsavelPeloEvento = input('Digite o nome do responsável pelo evento: ')
+funcionarioDaEmpresa = input('Digite no nome do funcionário da empresa: ')
+evento = input('Digite o nome do evento: ')
+valorDoEvento = float(input('Digite o valor do evento: '))
+
+print('Declaro para o senhor', responsavelPeloEvento, ', que o senhor', funcionarioDaEmpresa, 'esteve presente no evento', evento, 'e gastou o valor de R$', str(valorDoEvento), 'com a entrada.')
+
